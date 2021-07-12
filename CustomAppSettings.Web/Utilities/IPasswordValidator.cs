7af797ef421c0440a0e4558a5a64abe39172c0f4
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-12 18:01:22 +0200

    Added Utilities folder with an interface and a class

---
+++ CustomAppSettings.Web/Utilities/IPasswordValidator.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomAppSettings.Web.Utilities
+{
+    public interface IPasswordValidator
+    {
+        public string Pattern { get; set; }
+        public string PasswordString { get; set;}
+        public int PasswordLength { get; set;}
+    }
+}
---
+++ CustomAppSettings.Web/Utilities/PasswordValidator.cs
@@ -0,0 +1,10 @@
+﻿namespace CustomAppSettings.Web.Utilities
+{
+    public class PasswordValidator : IPasswordValidator
+    {
+        public string Pattern { get; set; }
+        public string PasswordString { get; set; }
+        public int PasswordLength { get; set; }
+    }
+}
+
