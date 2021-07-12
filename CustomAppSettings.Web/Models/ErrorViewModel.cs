442cd486280c491af298349e55828895dd363f5f
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-12 15:27:40 +0200

    Add project files.

---
+++ CustomAppSettings.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CustomAppSettings.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
