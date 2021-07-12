7159a411796c3536503b45b2b48514aa1ade6a82
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-12 15:58:42 +0200

    Added new model class

---
+++ CustomAppSettings.Web/Models/Cat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomAppSettings.Web.Models
+{
+    public class Cat
+    {
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; } 
+        public int NumberOfLegs { get; set; }
+    }
+}
