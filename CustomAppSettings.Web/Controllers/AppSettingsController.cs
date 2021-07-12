5fcb20d4ca40eea23882073d3ca70ae4697f5770
Beata Zalewa <beata.zalewa@gmail.com>
2021-07-12 22:03:24 +0200

    Added a new controllera and a first option how to read data from custom configuration

---
+++ CustomAppSettings.Web/Controllers/AppSettingsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Linq;
+
+namespace CustomAppSettings.Web.Controllers
+{
+    [Route("appsettings")]
+    public class AppSettingsController : Controller
+    {
+        protected readonly IConfiguration _configuration;
+
+        public AppSettingsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [Route("using-iconfiguration")]
+        public IActionResult UsingIConfiguration()
+        {
+            return Content(_configuration.GetValue<string>("PasswordValidator:Pattern"), "text/plain");
+        }
+
+    }
+
+}
